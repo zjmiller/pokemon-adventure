ec6704d52c0d9a814d6bc87a284109fa022727f3
Zachary Miller <zachary.john.miller@gmail.com>
2016-09-30 09:44:48 -0500

    Delete unused selectors folder

--- shared/selectors/locationIsUnoccupied.js
+++
@@ -1,9 +0,0 @@
-module.exports = function locationIsUnoccupied(state, location){
-  return !(
-    location.player
-    && location.npc
-    && location.berry
-    && location.mushroom
-    && location.gem
-  );
-}
